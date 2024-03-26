e36dbbbd66baee9342b02421a842c7c9316236c1
Niten_OS <benosuke@protonmail.com>
2024-03-26 16:53:43 +0100

    Nouveau son de pas, pleur de l'ado

--- Source/Project_P/adoChild.cpp
+++ Source/Project_P/adoChild.cpp
@@ -6,6 +6,10 @@
 AadoChild::AadoChild() {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	crySFX = CreateDefaultSubobject<UAudioComponent>(TEXT("CrySFX"));
+	crySFX->SetRelativeLocation(FVector(0.0f, 0.0f, -70.0f));
+	crySFX->SetupAttachment(RootComponent);
 }
 
 void AadoChild::BeginPlay() {
@@ -33,5 +37,23 @@ void AadoChild::Tick(float DeltaTime) {
 		}
 
 		count = 0;
+	}
+
+	// Cry 
+	{
+		crySFX->Sound = cry[numCry];
+
+		crycount += DeltaTime;
+		if (crycount >= maxCryCount) {
+
+			//numFootstep = FMath::RandRange(0, footsteps.Num() - 1);
+			numCry += 1;
+			if (numCry >= cry.Num()) { numCry = 0; }
+			//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::SanitizeFloat(numFootstep));
+
+			crySFX->Play();
+			crycount = 0.0f;
+		}
+
 	}
 }
--- Source/Project_P/adoChild.h
+++ Source/Project_P/adoChild.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "childBase.h"
+#include "Components/AudioComponent.h"
 #include "adoChild.generated.h"
 
 /**
@@ -17,16 +18,27 @@ class PROJECT_P_API AadoChild : public AchildBase
 public:
 	AadoChild();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ChildDeplacement)
+	TArray<USoundBase*> cry;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CPP Seting")
-	float adoWaitTime = 15.0f;
+	float adoWaitTime = 30.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CPP Seting")
 	float test;
 
 	int navPointCount = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CPPSound)
+	UAudioComponent* crySFX;
+
+	// footstep var
+	int numCry = 0;
+	float crycount = 0.0f;
+	float maxCryCount = 5.0f;
 
 public:
 	// Called every frame
